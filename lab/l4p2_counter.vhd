0b6418be84b107ea0067fedf4f3bcbbb281f8528
m47eus2 <158043802+m47eus2@users.noreply.github.com>
2026-05-16 17:33:20 +0200

    Update l4p2_counter.vhd

--- lab/l4p2_counter.vhd
+++ lab/l4p2_counter.vhd
@@ -15,8 +15,12 @@ entity top is
 end top;
 
 architecture behavioral of top is
+    signal from_counter : std_logic_vector(7 downto 0);
 begin
-    u1: entity work.mc_counter_8bit port map(sw(0),key(3),sw(1),led(7 downto 0));
+    u1: entity work.mc_counter_8bit port map(sw(0),key(3),sw(1),from_counter);
+    u2: entity work.mc_7seg_decoder_4bit port map(from_counter(3 downto 0), hex0);
+    u3: entity work.mc_7seg_decoder_4bit port map(from_counter(7 downto 4), hex1);
+    led(7 downto 0) <= from_counter;
 end behavioral;
 
 -- mc_counter_8bit
@@ -45,3 +49,37 @@ begin
     end process;
     q <= q_temp;
 end arch1;
+
+-- mc_7seg_decoder_4bit
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity mc_7seg_decoder_4bit is
+    port(
+        s : in std_logic_vector(3 downto 0);
+        y : out std_logic_vector(6 downto 0)
+    );
+end mc_7seg_decoder_4bit;
+
+architecture arch2 of mc_7seg_decoder_4bit is
+begin
+    with s select
+        y <= "1000000" when "0000",
+        "1111001" when "0001",
+        "0100100" when "0010",
+        "0110000" when "0011",
+        "0011001" when "0100",
+        "0010010" when "0101",
+        "0000010" when "0110",
+        "1111000" when "0111",
+        "0000000" when "1000",
+        "0010000" when "1001",
+        "0001000" when "1010",
+        "0000011" when "1011",
+        "1000110" when "1100",
+        "0100001" when "1101",
+        "0000110" when "1110",
+        "0001110" when "1111",
+        "1111111" when others;
+end arch2;
